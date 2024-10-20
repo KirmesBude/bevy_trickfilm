5dd82f0309a745521f25b6f57e839791ad0ebd88
KirmesBude <kirmesbude@gmail.com>
2024-10-20 23:51:14 +0200

    Optimize the way events are send/triggered

--- src/animation/event.rs
+++ src/animation/event.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, reflect::GetTypeRegistration, utils::HashMap};
 
 use crate::asset::AnimationClip2D;
 
@@ -8,7 +8,7 @@ use super::{
 };
 
 /// AnimationEvents are triggered by the animation system if registered as such with the App.
-pub trait AnimationEvent: Event + FromReflect {
+pub trait AnimationEvent: Event + GetTypeRegistration + FromReflect {
     /// Implement this to be able to set the entity for a targeted event.
     /// Default implementation is a No-Op.
     fn set_entity(&mut self, entity: Entity) {
@@ -21,46 +21,57 @@ pub fn default_entity() -> Entity {
     Entity::PLACEHOLDER
 }
 
-// Define generic trigger and event systems
-pub fn send_animation_event<T: AnimationEvent>(
+fn collect_events<T: AnimationEvent>(
+    animation_players: Query<(Entity, &AnimationPlayer2D)>,
+    animation_clips: &Assets<AnimationClip2D>,
+) -> HashMap<Entity, Vec<T>> {
+    animation_players
+        .iter()
+        .map(|(entity, animation_player)| {
+            let mut events = Vec::with_capacity(0);
+            if let Some(animation_clip) = animation_clips.get(animation_player.animation_clip()) {
+                // TODO: I need a better way to detect frame changes here
+                // Get all events for this frame transition, if any
+                if let Some(reflected_events) =
+                    animation_clip.events().get(&animation_player.frame())
+                {
+                    events.reserve(reflected_events.len());
+                    for reflected_event in reflected_events {
+                        // TODO: Is this the most efficient way?
+                        if let Some(mut event) = T::from_reflect(reflected_event.as_reflect()) {
+                            event.set_entity(entity);
+                            events.push(event);
+                        }
+                    }
+                }
+            }
+            (entity, events)
+        })
+        .collect()
+}
+
+fn send_animation_event<T: AnimationEvent>(
     mut event_writer: EventWriter<T>,
     animation_players: Query<(Entity, &AnimationPlayer2D)>,
     animation_clips: Res<Assets<AnimationClip2D>>,
 ) {
-    for (entity, animation_player) in &animation_players {
-        if let Some(animation_clip) = animation_clips.get(animation_player.animation_clip()) {
-            if let Some(reflected_events) = animation_clip.events().get(&animation_player.frame()) {
-                for reflected_event in reflected_events {
-                    // TODO: Patch in entity somehow
-                    if let Some(mut event) = T::from_reflect(reflected_event.as_reflect()) {
-                        // TODO: batch
-                        event.set_entity(entity);
-                        event_writer.send(event);
-                    }
-                }
-            }
-        }
+    let entity_event_map = collect_events::<T>(animation_players, &animation_clips);
+
+    for (_, events) in entity_event_map {
+        event_writer.send_batch(events);
     }
 }
 
-// TODO: generalize the query stuff
-pub fn trigger_animation_event<T: AnimationEvent>(
+fn trigger_animation_event<T: AnimationEvent>(
     mut commands: Commands,
     animation_players: Query<(Entity, &AnimationPlayer2D)>,
     animation_clips: Res<Assets<AnimationClip2D>>,
 ) {
-    for (entity, animation_player) in &animation_players {
-        if let Some(animation_clip) = animation_clips.get(animation_player.animation_clip()) {
-            if let Some(reflected_events) = animation_clip.events().get(&animation_player.frame()) {
-                for reflected_event in reflected_events {
-                    // TODO: Patch in entity somehow
-                    if let Some(mut event) = T::from_reflect(reflected_event.as_reflect()) {
-                        // TODO: batch
-                        event.set_entity(entity);
-                        commands.trigger(event);
-                    }
-                }
-            }
+    let entity_event_map = collect_events::<T>(animation_players, &animation_clips);
+
+    for (entity, events) in entity_event_map {
+        for event in events {
+            commands.trigger_targets(event, entity);
         }
     }
 }
@@ -76,6 +87,7 @@ pub trait AnimationEventAppExtension {
 
 impl AnimationEventAppExtension for App {
     fn add_animation_event<T: AnimationEvent>(&mut self) -> &mut Self {
+        self.add_event::<T>().register_type::<T>();
         self.add_systems(
             PostUpdate,
             send_animation_event::<T>
@@ -85,6 +97,7 @@ impl AnimationEventAppExtension for App {
     }
 
     fn add_animation_trigger<T: AnimationEvent>(&mut self) -> &mut Self {
+        self.register_type::<T>(); // add_event is not necessary for observers
         self.add_systems(
             PostUpdate,
             trigger_animation_event::<T>
